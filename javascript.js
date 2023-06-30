21aebc1ffc8535a1e847c012b13ffb8981b5ffe7
Santiago Blachere <gotregotregotre@gmail.com>
2023-06-30 17:17:16 -0300

    AGREGAR INTERACTIVIDAD ENTRE USUARIO Y DOM

--- javascript.js
+++ javascript.js
@@ -1,16 +1,22 @@
 let computerChoice;
+let userChoice;
+let result;
 let userWins = 0;
 let computerWins = 0;
+let ties = 0;
+let resetButtonCreated = false;
 
-function playRound () {
-let userChoice = prompt("Rock, paper or scissors?").toLowerCase()
+
+
+/* function playRound () {
+
 if (userChoice === "rock" || userChoice === "paper" || userChoice ==="scissors") {
     round(userChoice, getComputerChoice())
 
 } else {
     console.log("invalid input")
 }
-}
+} */
 function getComputerChoice() {
     let computerNumber =  Math.floor(Math.random() * 3)
     if (computerNumber === 1) {
@@ -24,44 +30,107 @@ function getComputerChoice() {
     
 }
 
-function round(userChoice, getComputerChoice) {
+function round(userChoice) {
     
     console.log(userChoice, computerChoice)
-    if (userChoice === "rock" && getComputerChoice === "Scissors" || 
-        userChoice === "paper" && getComputerChoice === "Rock" ||
-        userChoice === "scissors" && getComputerChoice === "Paper" ) {
+    if (userChoice === "rock" && computerChoice === "Scissors" || 
+        userChoice === "paper" && computerChoice === "Rock" ||
+        userChoice === "scissors" && computerChoice === "Paper" ) {
         
-        console.log("User wins!")
+        result = "User wins!"
         ++userWins
     } else if(
-        getComputerChoice === "Rock" && userChoice === "scissors" || 
-        getComputerChoice === "Paper" && userChoice === "rock" ||
-        getComputerChoice === "Scissors" && userChoice === "paper" ) {
+        computerChoice === "Rock" && userChoice === "scissors" || 
+        computerChoice === "Paper" && userChoice === "rock" ||
+        computerChoice === "Scissors" && userChoice === "paper" ) {
         
-        console.log("Computer wins!")
+        result = "Computer wins!"
         ++computerWins
     } else {
         
-        console.log("Its a tie!")
+        result = "Its a tie!"
+        ++ties
     }
     
 
 }
-function game() {
-    for (let index = 0; index < 5; index++) {
-        playRound()
-        
-    }
-    if (userWins > computerWins) {
-        alert("User wins!")
-    } else if (computerWins > userWins){
-        alert("Computer wins")
+
+
+const buttonRock = document.querySelector(".rock");
+const buttonPaper = document.querySelector(".paper");
+const buttonScissors = document.querySelector(".scissors");
+
+const resultRound =
+
+buttonRock.addEventListener('click', getChoice);
+buttonPaper.addEventListener('click', getChoice);
+buttonScissors.addEventListener('click', getChoice);
+
+
+function getChoice(e) {
+    e.stopPropagation();
+    
+    if ( e.target.className === "rock" ) {
+    userChoice = "rock"
+    } else if (e.target.className === "paper") {
+        userChoice = "paper"
     } else {
-        alert("Its a tie!")
+        userChoice = "scissors"
     }
+    getComputerChoice();
+    round(userChoice, computerChoice);
+    showResults();
+    winner();
+   
+}
+const body = document.querySelector("body")
+const roundResults = document.querySelector(".rounds");
+const finalResult = document.createElement("h1");
+body.appendChild(finalResult)
+
+function showResults() {
+    roundResults.innerHTML = `USER WINS: ${userWins} 
+    COMPUTER WINS: ${computerWins} 
+    TIES: ${ties}`
 }
 
-game();
+function winner() {
+    
+    if (userWins === 5) {
+        finalResult.innerText = "USER WINS!!!!"
+        if (resetButtonCreated === false) {
+            const reset = document.createElement("button");
+            reset.innerText = "RESETEAR"
+        
+            body.appendChild(reset)
+            resetButtonCreated = true;
+            reset.addEventListener ("click", () => {
+                userWins = 0;
+                computerWins = 0;
+                ties = 0;
+                showResults();
+            })
+            }
+    } else if (computerWins === 5) {
+        finalResult.innerText = "COMPUTER WINS!!! YOU SUCK ASS"
+        
+        if (resetButtonCreated === false) {
+            const reset = document.createElement("button");
+            reset.innerText = "RESETEAR"
+        
+            body.appendChild(reset)
+            resetButtonCreated = true;
+            reset.addEventListener ("click", () => {
+                userWins = 0;
+                computerWins = 0;
+                ties = 0;
+                showResults();
+            })
+            
+            }
+    } 
+}
+
 
 
 
